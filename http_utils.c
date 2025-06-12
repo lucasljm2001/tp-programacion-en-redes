6cb16d5a98bad24dcb2e87fafb72e139e77597f7
lucas.martinez2 <lucas.martinez@naranjax.com>
2025-06-12 12:10:03 -0300

    Agrego el control en el atender comun

--- http_utils.c
+++ http_utils.c
@@ -54,6 +54,13 @@ void* atenderCliente(void* args) {
         }
     } while (nbytes > 0 && totalRead < sizeof(buffer) - 1);
 
+    if (totalRead == 0) {
+        printf("No se recibió ninguna solicitud HTTP\n");
+        close(clientSocket);
+        free(args);
+        return NULL;
+    }
+
     HTTPRequest request = parse_request(buffer);
 
     printf("Método: %s\n", request.method);

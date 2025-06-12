d80f06f2f7f622f25f74804ebd32d21c9dccc1cf
lucas.martinez2 <lucas.martinez@naranjax.com>
2025-06-12 11:47:28 -0300

    fix: add error handling for socket, bind, and listen calls

--- server_multithread.c
+++ server_multithread.c
@@ -40,6 +40,11 @@ int main(int argc, char *argv[]) {
      * For Internet family of IPv4 addresses we use AF_INET
      */
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (listenfd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
     
 
     /* Do not wait to listener socket to be released
@@ -62,13 +67,30 @@ int main(int argc, char *argv[]) {
     /* The call to the function "bind()" assigns the details specified
      * in the structure serv_addr' to the socket created in the step above
      */
-    bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("bind");
+        close(listenfd);
+        exit(EXIT_FAILURE);
+    }
+    {
+        /* code */
+    }
+    
 
     /* The call to the function "listen()" with second argument as 10 specifies
      * maximum number of client connections that server will queue for this listening
      * socket.
      */
-    listen(listenfd, 10);
+    if (listen(listenfd, 10) < 0) {
+        perror("listen");
+        close(listenfd);
+        exit(EXIT_FAILURE);
+    }
+    {
+        /* code */
+    }
+    
+    
     
     printf("listening port %d\n", puerto);
     
